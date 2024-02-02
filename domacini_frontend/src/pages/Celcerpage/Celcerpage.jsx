d6c1c89bfd31aa19cae2f4c96947416ede7cd99a
Jaka Celcer <jaka.celcer@gmail.com>
2024-02-02 13:23:53 +0100

    moj page

--- domacini_frontend/src/pages/Celcerpage/Celcerpage.jsx
+++ domacini_frontend/src/pages/Celcerpage/Celcerpage.jsx
@@ -8,6 +8,23 @@ function CelcerPage() {
             <div className="title-container">
                 <h1>Welcome to celcer page</h1>
             </div>
+            <div className="naslov1">
+                <h2>Naslov 1</h2> 
+                  
+            </div>
+            <p className="vsabesedila">Tukaj pride besedilo</p>
+            <div className="naslov2">
+                <h2>Naslov 2</h2> 
+                
+                
+            </div>
+            <p className="vsabesedila">Tukaj pride besedilo</p>
+            <div className="naslov3">
+                <h2>Naslov 3</h2> 
+                
+                
+            </div>
+            <p className="vsabesedila">Tukaj pride besedilo</p>
         </div>
     )
 }
