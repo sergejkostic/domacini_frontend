96a6926945e5231d5b582adbd31243d36d98c8ec
Sergej Kostic <sergej.kostic04@gmail.com>
2025-10-18 15:28:46 +0200

    Revert "removed navigation;"
    This reverts commit d1c5347920ef17a9d2cd5e205ed8999688a88b78.

--- domacini_frontend/src/pages/Homepage/Homepage.jsx
+++ domacini_frontend/src/pages/Homepage/Homepage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import './Homepage.css';
 
 function HomePage() {
+    const navigate = useNavigate();
     
     return (
         <div className="main_container">
