fa8faf0260a7b16784ba9981e92d17514d7d1329
Sergej Kostic <sergej.kostic04@gmail.com>
2025-10-18 13:43:11 +0200

    card text change

--- domacini_frontend/src/pages/Celcerpage/Celcerpage.jsx
+++ domacini_frontend/src/pages/Celcerpage/Celcerpage.jsx
@@ -10,7 +10,7 @@ function CelcerPage() {
             <Card title="osnove rac "text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
             <Card title="swizz "text="not goated"/>
             <Card title="hoodied "text="shiet"/>
-            <Card title="xegi "text="goat"/>
+            <Card title="xegi "text="goat the best ever"/>
         </div>
 
     )
