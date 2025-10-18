8af82fc7eb101894ec84ff3a88ca2be45062bca0
Sergej Kostic <sergej.kostic04@gmail.com>
2025-10-18 13:58:16 +0200

    Added cards to Homepage.jsx;

--- domacini_frontend/src/pages/Homepage/Homepage.jsx
+++ domacini_frontend/src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import './Homepage.css';
+import Card from "../../Components/Card";
 
 function HomePage() {
     const navigate = useNavigate();
     
     return (
         <div className="main_container">
-                   
+            <Card title="osnove rac "text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
+            <Card title="swizz "text="not goated"/>
+            <Card title="hoodied "text="shiet"/>
+            <Card title="xegi "text="goat the best ever"/>
         </div>
     )
 }
